0d046c8c35f64e0d8d46abaf809241f82349ff21
user <user@users-MacBook-Pro.local>
2024-01-22 22:52:40 +0400

    finish email validation functionary and create navigation from login into navigationbar page and start password validation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Request from "./Request/Request";
 import NavigationBar from "./Request/Components/navigation/NavigationBar";
 import LogIn from "./Request/Components/login/LogIn";
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route,useNavigate } from 'react-router-dom'
 
 
 
--- src/Request/Components/login/LogIn.jsx
+++ src/Request/Components/login/LogIn.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { useState} from "react";
-import { Route, useNavigate, Routes,Link } from "react-router-dom";
-import NavigationBar from "../navigation/NavigationBar";
-
+import {useNavigate } from 'react-router-dom'
 
 
 const LogIn = () => {
@@ -10,7 +8,10 @@ const LogIn = () => {
    const [message, setMessage]= useState()
     const [password,setPassword] = useState("")
     const [checked, setChecked] = useState(false)
-    
+    const [error, setError] = useState(null)
+    const [errorMessage,setErrorMessage] = useState('')
+    const navigate = useNavigate();
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     const user =[
         {
            email,
@@ -19,47 +20,76 @@ const LogIn = () => {
            key: Date.now()
         }
     ]
-  const changePage= () =>{
-    console.log("navigayion") 
-    return(
-        <Link to="NavigationBar"></Link>  
-        
-    )
-  }
-  const emailValidation = ()=>{
-    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-     if(setEmail == ""){
-                setMessage("please enter the email!")
-               console.log("empty")
-               }
-               if(emailRegex.test(email)){
-                setMessage("error.use correct email!")
+ 
+  const emailValidation = ()=>{ 
+     if(email === ""){
+        setMessage("please enter the email!")
+        console.log("empty")
+        }
+    if(!emailRegex.test(email)){
+       setMessage("error.use correct email!")
                 
-              }else{
-                setMessage("what?")
-            }  
+    }else{
+        setMessage("what?")
+    }  
 
   }
+  const passwordValidation = (e) => {
+    const new_pass = e.target.value;
+      setPassword(new_pass);
+        
+    
+      var lowerCase = /[a-z]/g;
+      var upperCase = /[A-Z]/g;
+      var numbers = /[0-9]/g;
+      if (!new_pass.match(lowerCase)) {
+         setErrorMessage("Password should contains lowercase letters!");
+      } else if (!new_pass.match(upperCase)) {
+         setErrorMessage("Password should contain uppercase letters!");
+      } else if (!new_pass.match(numbers)) {
+         setErrorMessage("Password should contains numbers also!");
+      } else if (new_pass.length < 8) {
+         setErrorMessage("Password length should be more than 8.");
+      } else {
+         setErrorMessage("Password is strong!"); 
+        
+      }
+  }
+ 
   const foo = () => { 
+    try{
+         emailValidation()
+    
         const checkLocalstorage = JSON.parse(localStorage.getItem("users"))
-        emailValidation()
         if(checkLocalstorage){
             {checkLocalstorage.find((item)=>{  
                if(item.email === email) {
                 console.log("this email already ragac")
                 return
                }
-               if(item.email !== email){
+               if(item.email !== email && emailRegex.test(email)){
                 const userArray = [...user,...checkLocalstorage]
                 localStorage.setItem("users", JSON.stringify(userArray)); 
-                console.log("Email is valid")
+                setMessage("Email is valid")
+                return
                } 
+               
          })
         }
         }else{
           localStorage.setItem("users", JSON.stringify(user));  
         }
+    }catch(error){
+       setError("there is a error")
+        return error
+    }
+       if (!error && email !=="" && emailRegex.test(email) ){
+        navigate("/NavigationBar")
+        
+       }
+        
   };
+  
 
             const deleteItem = () => {
                 const checkLocalstorage = JSON.parse(localStorage.getItem("users"))
@@ -75,8 +105,9 @@ const LogIn = () => {
         <form>
            
             <input type="email" placeholder="email" key ="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>  
-            <input type="password" placeholder="password" id="password" key="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-            <h1>{message}</h1>
+            <input type="password" placeholder="password:Abc.@678" id="password" key="password" value={password} onChange={passwordValidation}/>
+            <h1>{message}{errorMessage}</h1>
+
             <label>  
               <input type="checkBox" checked= {checked} onChange={(e) => setChecked(e.target.checked)}/>
               {""}
